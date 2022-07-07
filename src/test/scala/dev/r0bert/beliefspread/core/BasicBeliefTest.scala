7023bbe40653a2e26c9af4c288687deeed4c8ad0
Robert Greener <me@r0bert.dev>
2022-07-07 12:07:07 +0100

    BasicBeliefTest: add setRelationship when exists
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/test/scala/dev/r0bert/beliefspread/core/BasicBeliefTest.scala
+++ src/test/scala/dev/r0bert/beliefspread/core/BasicBeliefTest.scala
@@ -186,4 +186,14 @@ class BasicBeliefTest extends munit.FunSuite {
       b1.setRelationship(b2, Some(-2.0))
     }
   }
+
+  test("setRelationship when exists") {
+    val b1 = BasicBelief("b1")
+    val b2 = BasicBelief("b2")
+    val relationship = HashMap[Belief, Double]()
+    relationship.put(b2, 0.2)
+    FieldUtils.writeField(b1, "relationship", relationship, true)
+    b1.setRelationship(b2, Some(0.5))
+    assertEquals(relationship.get(b2), Some(0.5))
+  }
 }
