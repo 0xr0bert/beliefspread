7673b40cc723a0e1757da02c0e0bdf53140e5840
Robert Greener <me@r0bert.dev>
2022-07-08 16:21:42 +0100

    BasicBehaviour: Fix scaladoc links
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/main/scala/dev/r0bert/beliefspread/core/BasicBehaviour.scala
+++ src/main/scala/dev/r0bert/beliefspread/core/BasicBehaviour.scala
@@ -2,14 +2,14 @@ package dev.r0bert.beliefspread.core
 
 import java.util.UUID
 
-/** A [BasicBehaviour] is an implementation of [Behaviour].
+/** A [[BasicBehaviour]] is an implementation of [[Behaviour]].
   *
   * @param name
-  *   The name of the [BasicBehaviour].
+  *   The name of the [[BasicBehaviour]].
   * @param uuid
-  *   The [UUID] of the [BasicBehaviour].
+  *   The [[UUID]] of the [[BasicBehaviour]].
   * @constructor
-  *   Create a new [BasicBehaviour] with a supplied `name` and `uuid`.
+  *   Create a new [[BasicBehaviour]] with a supplied `name` and `uuid`.
   * @author
   *   Robert Greener
   * @since v0.14.0
@@ -17,27 +17,27 @@ import java.util.UUID
 class BasicBehaviour(override var name: String, override var uuid: UUID)
     extends Behaviour {
 
-  /** Create a new [BasicBehaviour] with a random [UUID].
+  /** Create a new [[BasicBehaviour]] with a random [[UUID]].
     *
-    * [UUID.randomUUID] is used to randomly generate the UUID.
+    * [[UUID.randomUUID]] is used to randomly generate the UUID.
     *
     * @param name
-    *   The `name` of the [BasicBehaviour].
+    *   The `name` of the [[BasicBehaviour]].
     * @author
     *   Robert Greener
     * @since v0.14.0
     */
   def this(name: String) = this(name, UUID.randomUUID)
 
-  /** Compare equality between this [BasicBehaviour] and another
-    * [BasicBehaviour]
+  /** Compare equality between this [[BasicBehaviour]] and another
+    * [[BasicBehaviour]]
     *
-    * They are equal iff. the [UUID] is equal.
+    * They are equal iff. the [[UUID]] is equal.
     *
     * @param other
-    *   The other [Any].
+    *   The other [[Any]].
     * @return
-    *   true If the [UUID] is equal.
+    *   true If the [[UUID]] is equal.
     * @author
     *   Robert Greener
     * @since v0.14.0
@@ -47,9 +47,9 @@ class BasicBehaviour(override var name: String, override var uuid: UUID)
     case _                     => false
   }
 
-  /** Get the `hashCode` of the [BasicBelief].
+  /** Get the `hashCode` of the [[BasicBelief]].
     *
-    * This is solely based on the [UUID].
+    * This is solely based on the [[UUID]].
     *
     * @return
     *   The hashCode.
